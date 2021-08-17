c5bafd6e0b10911372f0e933ad703b965d2fd3ac
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-17 22:24:38 +0900

    Stage 2/6: Expand the search

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,11 +1,26 @@
 package search
 
 fun main() {
-    val words = readLine()!!.split(" ")
-    val i = words.indexOf(readLine()!!)
-    if (i != -1) {
-        println(i + 1)
-    } else {
-        println("Not found")
+    println("Enter the number of people:")
+    val number = readLine()!!.toInt()
+    val db = mutableListOf<String>()
+    repeat(number) {
+        db.add(readLine()!!)
+    }
+    println("Enter the number of search queries:")
+    val queries = readLine()!!.toInt()
+    repeat(queries) {
+        println("Enter data to search people:")
+        val man = readLine()!!
+
+        val found = db.filter { it.lowercase().contains(man.lowercase()) }
+        if (found.isEmpty()) {
+            println("No matching people found.")
+        } else {
+            println("People found:")
+            for (el in found) {
+                println(el)
+            }
+        }
     }
 }
