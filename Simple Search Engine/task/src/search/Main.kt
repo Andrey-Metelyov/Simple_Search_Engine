c2926c3f33463318797e9447382321090a444304
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-17 22:32:06 +0900

    Stage 3/6: User menu

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,26 +1,72 @@
 package search
 
+val db = mutableListOf<String>()
+
 fun main() {
+    fillDb()
+
+    while (true) {
+        when (menu()) {
+            1 -> findPerson()
+            2 -> printAll()
+            0 -> break
+        }
+    }
+
+    println("Bye!")
+
+//    println("Enter the number of search queries:")
+//    val queries = readLine()!!.toInt()
+//    repeat(queries) {
+//        println("Enter data to search people:")
+//        val man = readLine()!!
+//
+//    }
+}
+
+fun printAll() {
+    println("=== List of people ===")
+    for (record in db) {
+        println(record)
+    }
+}
+
+fun findPerson() {
+    println("Enter a name or email to search all suitable people.")
+    val input = readLine()!!
+    val found = db.filter { it.lowercase().contains(input.lowercase()) }
+    if (found.isEmpty()) {
+        println("No matching people found.")
+    } else {
+//        println("People found:")
+        for (el in found) {
+            println(el)
+        }
+    }
+}
+
+private fun menu(): Int {
+    val options = arrayOf("1", "2", "0")
+    while (true) {
+        println(
+            "=== Menu ===\n" +
+                    "1. Find a person\n" +
+                    "2. Print all people\n" +
+                    "0. Exit"
+        )
+        val choice = readLine()!!
+        if (choice !in options) {
+            println("Incorrect option! Try again.")
+        } else {
+            return choice.toInt()
+        }
+    }
+}
+
+private fun fillDb() {
     println("Enter the number of people:")
     val number = readLine()!!.toInt()
-    val db = mutableListOf<String>()
     repeat(number) {
         db.add(readLine()!!)
-    }
-    println("Enter the number of search queries:")
-    val queries = readLine()!!.toInt()
-    repeat(queries) {
-        println("Enter data to search people:")
-        val man = readLine()!!
-
-        val found = db.filter { it.lowercase().contains(man.lowercase()) }
-        if (found.isEmpty()) {
-            println("No matching people found.")
-        } else {
-            println("People found:")
-            for (el in found) {
-                println(el)
-            }
-        }
     }
 }
